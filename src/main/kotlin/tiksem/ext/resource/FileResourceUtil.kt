8fb34be875f7aca1540e013ec4a3bd338aa76ef9
Semyon Tikhonenko <semyontikhonenko@gmail.com>
2024-04-07 00:31:48 +0200

    FileResourceUtil added

---
+++ src/main/kotlin/tiksem/ext/resource/FileResourceUtil.kt
@@ -0,0 +1,32 @@
+package tiksem.ext.resource
+
+import java.io.File
+import java.io.InputStream
+
+object FileResourceUtil {
+    private val classLoader by lazy {
+        FileResourceUtil::class.java.classLoader
+    }
+
+    fun getResourceAsText(path: String): String? {
+        return classLoader.getResource(path)?.readText()
+    }
+
+    fun getResourceAsInputStream(path: String): InputStream? {
+        return classLoader.getResourceAsStream(path)
+    }
+
+    fun getResourceAsByteArray(path: String): ByteArray? {
+        return classLoader.getResource(path)?.readBytes()
+    }
+
+    fun getResourceAbsolutePath(path: String): String? {
+        return classLoader.getResource(path)?.path
+    }
+
+    fun getResourceFile(path: String): File? {
+        return classLoader.getResource(path)?.path?.let {
+            File(it)
+        }
+    }
+}
